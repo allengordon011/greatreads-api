dc4a792ed75f9ff8bdf131d3ed48611210c1698b
trig010 <trig.code@gmail.com>
2017-01-25 18:12:51 -0600

    GET working

--- greatreads-api.py
+++ greatreads-api.py
@@ -2,10 +2,35 @@
 
 import requests
 import json
+from xml.etree import ElementTree as ET
 
-api_key = key=nt4K4tcITuTOR11SDbX1Mg
-user_id_allen = 11210913
+api_key = 'key=nt4K4tcITuTOR11SDbX1Mg'
+user_id_allen = '11210913'
 
-get-read-shelf = requests.get('https://www.goodreads.com/review/list?format=xml&id=' + user_id_allen + '&shelf=read&sort=date_read&v=2&' + api_key)
 
-# print(get-read-shelf)
+# get_read_shelf = requests.get('https://www.goodreads.com/review/list?format=xml&id=' + user_id_allen + '&shelf=read&sort=date_read&v=2&' + api_key)
+#
+# # print(get_read_shelf)
+#
+# #get a series by author id
+# #use find author id by name
+author_id = 227840
+# get_series_author = requests.get('https://www.goodreads.com/series/list/' + author_id + '.xml?' + api_key)
+#
+# #get review by title and (optional) author
+title = 'A+GAME+OF+THRONES+MARTIN'
+# author = 'George R. R. Martin'
+# get_reviews = requests.get('https://www.goodreads.com/book/' + title + '.FORMAT')
+
+#search by title, author, or isbn
+
+get_books = requests.get('https://www.goodreads.com/search.xml?' + api_key + '&q=' + title)
+root = ET.fromstring(get_books.content)
+books = []
+
+for title in root[1].iter('title'):
+    books.append(title.text)
+for url in root[1].iter('image_url'):
+    book_cover = url.text
+
+print(books[:2])
