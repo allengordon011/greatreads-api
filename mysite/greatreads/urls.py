d2717c26e60534c7a3079d464cf21ea49d04b572
trig010 <trig.code@gmail.com>
2017-01-26 22:52:16 -0600

    added text input form but not functioning

--- mysite/greatreads/urls.py
+++ mysite/greatreads/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
-    url(r'^(?P<search_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    url(r'^(?P<search_id>[0-9]+)/submit/$', views.submit, name='submit'),
 ]
 
 #'the hard way'
--- mysite/greatreads/views.py
+++ mysite/greatreads/views.py
@@ -4,40 +4,39 @@ from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
 
-from .models import Choice, Search
+from .models import Choice, Search, Input
 
 class IndexView(generic.ListView):
     template_name = 'greatreads/index.html'
     context_object_name = 'latest_search_list'
 
     def get_queryset(self):
-        """Return the last five published searches.  (not including those set to be published in the future)."""
+        """Return the last five published searches. (not including those set to be published in the future)."""
         return Search.objects.filter(
             pub_date__lte=timezone.now()
             ).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Search
-    template_name = 'greatreads/detail.html'
+    template_name = 'greatreads/search.html'
 
 
 class ResultsView(generic.DetailView):
     model = Search
     template_name = 'greatreads/results.html'
 
-def vote(request, search_id):
+def submit(request, search_id):
     search = get_object_or_404(Search, pk=search_id)
     try:
-        selected_choice = search.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
+        selected_input = search.input_set.get(pk=request.POST['input'])
+    except (KeyError, Input.DoesNotExist):
         # Redisplay the search voting form.
-        return render(request, 'greatreads/detail.html', {
+        return render(request, 'greatreads/search.html', {
             'search': search,
-            'error_message': "You didn't select a choice.",
+            'error_message': "You didn't submit anything.",
         })
     else:
-        selected_choice.votes += 1
-        selected_choice.save()
+        selected_input.save()
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
